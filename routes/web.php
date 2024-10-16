2596c5dc8077328fc88fff5b14e8cb23c63e8608
Adanparedes123 <maycrydante416@gmail.com>
2024-10-16 16:14:02 -0400

    commit

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\p_principal\P_almacenController;
 use App\Http\Controllers\p_principal\P_tiendaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () { return view('welcome');});
+//Route::get('/', function () { return view('welcome');});
+Route::get('/', function () { return view('auth/login');});
 //Route::get('/', function () { return view('index');});
 //Route::view('/','layouts/welcome')/* ->middleware('auth') */;
-
 //oute::view('/', 'layouts/welcome');
-Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/af',[P_controller::class,'vis_af']);
@@ -20,4 +20,4 @@ Route::get('/almacen',[P_almacenController::class,'vis_almacen']);
 Route::get('/tienda',[P_tiendaController::class,'vis_tienda']);
 Route::get('/loginn',[P_tiendaController::class,'loginn']);
 Route::get('/registerr',[P_tiendaController::class,'registerr']);
-
+Auth::routes();
